02271da021a262ee4e9090d8f282a0a818fccf1f
MANUEL025 <131418423+MANUEL025@users.noreply.github.com>
2023-08-03 20:31:28 -0500

    ok

--- views/products/index.php
+++ views/products/index.php
@@ -1,19 +1,19 @@
 <style>
     .product-img-container {
-    width: 100%;
-    height: 200px; 
-    overflow: hidden;
-    
-}
+        width: 100%;
+        height: 200px;
+        overflow: hidden;
+
+    }
 </style>
 
 <div class="row">
-<a href="?c=companies&m=create" class="btn btn-primary">Crear nuevo compañia</a>
+    <a href="?c=companies&m=create" class="btn btn-primary">Crear nuevo compañia</a>
     <div class="col-md-6 col-lg-4 col-xl-3">
         <div class="card product-box">
             <div class="card-body">
-            
-                <div class="product-action" >
+
+                <div class="product-action">
                     <a href="?c=products&m=edit" class="btn btn-success btn-xs waves-effect waves-light"><i
                             class="mdi mdi-pencil"></i></a>
                     <a href="?c=products&m=edit" class="btn btn-danger btn-xs waves-effect waves-light"><i
@@ -30,16 +30,16 @@
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
-                        
+
                     </div> <!-- end row -->
                 </div> <!-- end product info-->
             </div>
@@ -62,20 +62,20 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
-                        
+
                     </div> <!-- end row -->
                 </div> <!-- end product info-->
             </div>
@@ -98,20 +98,20 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
-                        
+
                     </div> <!-- end row -->
                 </div> <!-- end product info-->
             </div>
@@ -134,16 +134,16 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
@@ -169,16 +169,16 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
@@ -199,21 +199,22 @@
                 </div>
 
                 <div class="bg-light product-img-container">
-                    <img src="assets/images/products/piso laminado Harbour Oak Grey.png" alt="product-pic" class="img-fluid">
+                    <img src="assets/images/products/piso laminado Harbour Oak Grey.png" alt="product-pic"
+                        class="img-fluid">
                 </div>
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
@@ -239,16 +240,16 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
@@ -274,23 +275,23 @@
 
                 <div class="product-info">
                     <div class="row align-items-center">
-                    <div class="col">
+                        <div class="col">
                             <h5 class="font-16 mt-0 sp-line-1"><a href="ecommerce-product-detail.html"
                                     class="text-dark">Pared 3D fibra vegetal caja x 4 paneles </a> </h5>
                             <div class="text-warning mb-2 font-13">
-                            <div class="col-auto">
-                            <div class="product-price-tag">
-                                $50.000
-                            </div>
-                        </div>
-                               
+                                <div class="col-auto">
+                                    <div class="product-price-tag">
+                                        $50.000
+                                    </div>
+                                </div>
+
                             </div>
                             <h5 class="m-0"> <span class="text-muted"> Stocks : 98 pcs</span></h5>
                         </div>
                     </div> <!-- end row -->
                 </div> <!-- end product info-->
                 <hr>
-               
+
             </div>
         </div> <!-- end card-->
     </div>
